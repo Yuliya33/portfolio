d6ebe039d44bf88dbcc1d9b175da59f2d3d59606
Yuliya33 <yuliya.polyakova1131@gmail.com>
2023-03-14 11:47:55 -0400

    correction de checkip

--- src/components/CheckIp.js
+++ src/components/CheckIp.js
@@ -22,8 +22,12 @@ export default function CheckIp() {
   const getData = async () => {
     const res = await axios.get('https://geolocation-db.com/json/')
 
+    let myIp = "142.184.85.102";
+    let ip = res.data.IPv4;
+    if(ip===myIp) ip = "My notebook";
+
     const ipData = {
-      ip: res.data.IPv4,
+      ip: ip,
       country: res.data.country_name,
       city: res.data.city,
       state: res.data.state,
